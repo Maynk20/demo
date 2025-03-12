c3dfd9eb7954a97fc0296f6d96dac030e9ee1ee0
Maynk20 <thakkarmayank26@gmail.com>
2025-03-12 23:28:08 +0530

    this is pho file.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Registration</title>
+</head>
+<body>
+    <h2>Student Registration Form</h2>
+    <form action="insert.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+
+        <label for="phone">Phone:</label>
+        <input type="text" name="phone" required><br><br>
+
+        <label for="course">Course:</label>
+        <input type="text" name="course" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+
+</body>
+</html>
